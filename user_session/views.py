e9833c496c03c12de8e57f2838b27de69e646440
Anurag Pandey <108535343+anurag-p15@users.noreply.github.com>
2024-03-27 16:19:28 +0530

    Added authetication

--- user_session/views.py
+++ user_session/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from pymongo import MongoClient
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from .models import User
+from django.contrib.auth import authenticate, login as auth_login
+
 
 
 def register(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
         username = request.POST.get('username')
         email = request.POST.get('username')  # Assuming 'username' is actually 'email'
         password = request.POST.get('password')
@@ -21,7 +22,7 @@ def register(request):
                 error_message = 'Email already exists'
                 return render(request, 'register.html', {'error': error_message})
 
-            user = User(name=name, username=username, password=hashed_password, bio=bio)
+            user = User(username=username, password=hashed_password, bio=bio)
             user.save()
 
             # Redirect to a success page or any other desired action
@@ -37,10 +38,33 @@ def register(request):
         return render(request, 'register.html')
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        # Retrieve user from the database using username
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # User does not exist
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error': error_message})
 
-
+        # Check if the entered password matches the one stored in the database
+        if check_password(password, user.password):
+            # Passwords match, authenticate user and redirect to dashboard
+          #  auth_login(request, user)
+            message='Hello eRROR HERE !!'
+            # No need to manually set last_login for MongoDB user model
+            request.session['user_id'] = user.id
+            return render(request, 'dashboard.html', {'user_data': user})
+        else:
+            # Passwords don't match
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error': error_message})
+    else:
+        return render(request, 'login.html')
+    
 def dashboard(request):
     return render(request,'dashboard.html')
 
